f6611e85fa1c1274305d84ebbd964f75e7161f93
clarabrandt <mariaclarabrandt@gmail.com>
2018-09-14 22:22:11 +0200

    Hero object- declare hero object

---
+++ game_logic.js
@@ -0,0 +1,12 @@
+// Write your JS here
+const hero = {
+    name: 'Maximus',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'fork',
+        damage: 2,
+    }
+};
+
