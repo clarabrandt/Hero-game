60ab1e922dcd82b58b6ca5e7863b0363b666e276
clarabrandt <mariaclarabrandt@gmail.com>
2018-09-14 22:23:59 +0200

    Hero object- create hero name

--- game_logic.js
+++ game_logic.js
@@ -1,12 +1,5 @@
 // Write your JS here
 const hero = {
     name: 'Maximus',
-    heroic: true,
-    inventory: [],
-    health: 10,
-    weapon: {
-        type: 'fork',
-        damage: 2,
-    }
 };
 
